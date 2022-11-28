98538809bbdb338ad363d82267c6b2ee2c77eaf3
NMSamDev <c_s_m_h_13@hotmail.com>
2022-11-28 16:57:37 -0500

    ViewModels

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/coin_list/CoinListViewModel.kt
@@ -0,0 +1,43 @@
+package com.plcoding.cryptocurrencyappyt.presentation.coin_list
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.cryptocurrencyappyt.common.Resource
+import com.plcoding.cryptocurrencyappyt.domain.use_case.get_coins.GetCoinsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class CoinListViewModel @Inject constructor(
+    private val getCoinsUseCase: GetCoinsUseCase
+): ViewModel() {
+    // The reason why I choose to have a private and a public version of the state is because
+    // I want to make sure that the state is only changed by the view model and not by the view.
+    private val _state = mutableStateOf(CoinListState())
+    val state: State<CoinListState> = _state
+
+    init {
+        getCoins()
+    }
+
+    private fun getCoins() {
+        getCoinsUseCase().onEach { result ->
+            when(result) {
+                is Resource.Success -> {
+                    _state.value = CoinListState(coins = result.data ?: emptyList())
+                }
+                is Resource.Error -> {
+                    _state.value = CoinListState(error = result.message ?: "An unexpected error occurred")
+                }
+                is Resource.Loading -> {
+                    _state.value = CoinListState(isLoading = true)
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
